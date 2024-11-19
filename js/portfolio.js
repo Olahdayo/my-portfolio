2f9d78799aa776301fcd88fb854a894691572ae0
Oladayo <blessinoloyede@gmail.com>
2024-11-19 06:49:33 +0100

    Genesis commit: A personal portfolio website

---
+++ js/portfolio.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to My Portfolio');
+});
+let currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
